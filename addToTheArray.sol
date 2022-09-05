fca008e1780db8cda22261522a81beb803107ad1
Eskandar Atrakchi <102361045+EskandarAtrakchi@users.noreply.github.com>
2022-09-05 05:48:07 +0100

    Store/review
    contract SimpleStorage
    Store/review

--- addToTheArray.sol
+++ addToTheArray.sol
@@ -1,29 +1,32 @@
 //SPDX-License-Identifier: MIT
+//here is to declare the virsion
 pragma solidity 0.8.8;
 
 contract SimpleStorage {
     //This gets initilized to zero 
-    uint256 ContractNumber;
+    uint256 favoriteNumber;
+    mapping(string => uint256) public nameToFavoriteNumber;
 
     struct People {
-        uint256 ContractNumber;
+        uint256 favoriteNumber;
         string name;
     }
 
-    //uint256[] public ContractNumberOne;
+    //uint256[] public favoritNumberList;
     People[] public people;
 
-    function store(uint256 ContractNumberOne) public {
-        ContractNumber = ContractNumberOne;
+    function store(uint256 _favoriteNumber) public {
+        favoriteNumber = _favoriteNumber;
         
-    }
-    function retrieve() public view returns(uint256){
-        return ContractNumber;
+    } 
+    function retrieve() public view returns(uint256) {
+        return favoriteNumber;
     }
 
     //adding to the array, People[] public people;
-    function addPerson(string memory _name, uint256 ContractNumberOne) public {
-        people.push(People(ContractNumberOne, _name));
+    function addPerson(string memory _name, uint256 _favoriteNumber) public {
+        people.push(People(_favoriteNumber, _name));
+        nameToFavoriteNumber[_name] = _favoriteNumber;
 
     }
 }
