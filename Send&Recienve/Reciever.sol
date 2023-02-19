fbb3d2b1f119a9d0e274d18bcd0738196057f1c3
Eskandar Atrakchi <102361045+EskandarAtrakchi@users.noreply.github.com>
2023-02-19 00:23:22 +0000

    .

--- Send&Recienve/Reciever.sol
+++ Send&Recienve/Reciever.sol
@@ -1,9 +1,4 @@
-pragma solidity ^0.8.0;
-
-interface IERC20 {
-    function balanceOf(address account) external view returns (uint);
-    function transfer(address recipient, uint amount) external returns (bool);
-}// SPDX-License-Identifier: GPL-3.0
+// SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.0;
 
 interface IBEP20 {
