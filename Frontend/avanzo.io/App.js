f6d81a5f6d90d1f944dec6b2def9a4264164c615
Eskandar Atrakchi <102361045+EskandarAtrakchi@users.noreply.github.com>
2023-06-25 01:12:47 +0100

    .

--- Frontend/avanzo.io/App.js
+++ Frontend/avanzo.io/App.js
@@ -17,3 +17,10 @@ document.getElementById("mySidenav").style.width = "250px";
 function closeNav() {
 document.getElementById("mySidenav").style.width = "0";
 }
+
+
+/*
+cancelPool
+FundFactory
+Vote
+*/
